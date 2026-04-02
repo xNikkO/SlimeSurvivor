36f7c7dd6940d7fb8df81af15aece8ea22454af8
xNikkO <122922233+xNikkO@users.noreply.github.com>
2026-04-03 01:25:23 +0200

    Last Update

--- menu.gd
+++ menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/slime_survivor_l1.tscn")
